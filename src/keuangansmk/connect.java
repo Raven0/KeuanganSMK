ed339a23410b9a8c1284d948b0663fad9a1b5207
Ismail Rohaga <azh.zhafir@gmail.com>
2017-11-20 09:10:50 +0700

    Main menu
    first commit

--- src/keuangansmk/connect.java
+++ src/keuangansmk/connect.java
@@ -9,13 +9,6 @@ package keuangansmk;
  *
  * @author azhzh
  */
-public class KeuanganSMK {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class connect {
     
 }
